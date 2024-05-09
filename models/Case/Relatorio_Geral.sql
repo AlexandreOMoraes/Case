42606c5a6d03dbc56e58cab170cc3e36aecc5991
Alexandre Moraes <Alexandre_oliveira16@outlook.com>
2024-05-09 22:10:51 +0000

    criado tabela da verdade

---
+++ models/Case/Relatorio_Geral.sql
@@ -0,0 +1,19 @@
+SELECT  ID_Venda,Quantidade_Vendida,
+        Data_Venda,
+        Metodo_Pagamento,
+        Total_Venda,
+        Data_Atual,
+        Nome_Produto,
+        Descricao_Produto,
+        Categoria,
+        Marca,
+        Sexo,
+        Idade,
+        Nome_Loja
+        Estado,
+        Cidade
+        from {{ref('Vendas_Sanitizada')}} as vendas
+        left join {{ref('Produtos_Sanitizada')}} as produtos on vendas.ID_Produto = produtos.ID_Produto
+        left join {{ref('Clientes_Sanitizada')}} as clientes on vendas.ID_Cliente = clientes.ID_Cliente
+        left join {{ref('Locais_Sanitizada')}} as locais on vendas.ID_Local = locais.ID_Loja
+        order by ID_Venda
