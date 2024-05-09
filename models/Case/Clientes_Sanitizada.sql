fdd815dc258aef1926bec7270e9057ab71a73b88
Alexandre Moraes <Alexandre_oliveira16@outlook.com>
2024-05-09 00:14:39 +0000

    teste

--- models/Case/Clientes_Sanitizada.sql
+++ models/Case/Clientes_Sanitizada.sql
@@ -5,9 +5,10 @@ with tabela1 as (
 select  ID_Cliente,
         Nome_Cliente,
         REGEXP_EXTRACT(Nome_Cliente, r'^\S+') AS Primeiro_Nome,
+        REGEXP_REPLACE(Nome_Cliente, r'^\S+\s*', '') AS Sobrenome, 
         Email,
         Telefone,
         Sexo,
         Data_Nascimento,
-        date_diff(Data_Atual,Data_Nascimento, year) as Idade,        REGEXP_REPLACE(Nome_Cliente, r'^\S+\s*', '') AS Sobrenome
+        date_diff(Data_Atual,Data_Nascimento, year) as Idade 
 from tabela1
